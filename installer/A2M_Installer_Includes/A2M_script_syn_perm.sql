d36f7bcb0122a34bc957b079e6ae518acfe05057
Tim Baird <tim@timbaird.com>
2017-09-26 11:32:41 +1000

    Reworked creation of synonyms and grants in A2M_script_syn_perm.sql

--- installer/A2M_Installer_Includes/A2M_script_syn_perm.sql
+++ installer/A2M_Installer_Includes/A2M_script_syn_perm.sql
@@ -6,21 +6,63 @@ SYNONYMS AND PERMISSIONS FOR MARKING SCRIPT
 
 -- SYNONYMS
 
+set serveroutput on;
 
-BEGIN
-EXECUTE IMMEDIATE 'drop public synonym A2M_CLEAN_UP';
-EXECUTE IMMEDIATE 'create public synonym A2M_CLEAN_UP  for ' || USER || '.A2M_CLEAN_UP';
-
-EXECUTE IMMEDIATE 'drop public synonym A2M_Mark_Ds_Ass2';
-EXECUTE IMMEDIATE 'create public synonym A2M_Mark_Ds_Ass2  for ' || USER || '.A2M_Mark_Ds_Ass2';
-
-EXECUTE IMMEDIATE 'drop public synonym A2M_db_object';
-EXECUTE IMMEDIATE 'create public synonym A2M_db_object  for ' || USER || '.A2M_db_object';
-
-EXECUTE IMMEDIATE 'drop public synonym A2M_assignment';
-EXECUTE IMMEDIATE 'create public synonym A2M_assignment  for ' || USER || '.A2M_assignment';
-
-end;
+begin
+    EXECUTE IMMEDIATE 'drop public synonym A2M_CLEAN_UP';
+exception
+    when others then
+    DOPL('DROP SYNONYM A2M_CLEAN_UP: ' || SQLERRM);
+END;
+/
+begin
+    EXECUTE IMMEDIATE 'create public synonym A2M_CLEAN_UP  for ' || USER || '.A2M_CLEAN_UP';
+exception
+    when others then
+    DOPL('CREATE SYNONYM A2M_CLEAN_UP: ' || SQLERRM);
+END;
+/    
+begin
+    EXECUTE IMMEDIATE 'drop public synonym A2M_Mark_Ds_Ass2';
+exception
+    when others then
+    DOPL('DROP SYNONYM A2M_Mark_Ds_Ass2: ' || SQLERRM);
+END;
+/  
+begin
+    EXECUTE IMMEDIATE 'create public synonym A2M_Mark_Ds_Ass2  for ' || USER || '.A2M_Mark_Ds_Ass2';
+exception
+    when others then
+    DOPL('CREATE SYNONYM A2M_Mark_Ds_Ass2: ' || SQLERRM);
+END;
+/
+begin
+    EXECUTE IMMEDIATE 'drop public synonym A2M_db_object';
+exception
+    when others then
+    DOPL('DROP SYNONYM A2M_db_object: ' || SQLERRM);
+END;
+/   
+begin
+    EXECUTE IMMEDIATE 'create public synonym A2M_db_object  for ' || USER || '.A2M_db_object';
+exception
+    when others then
+    DOPL('CREATE SYNONYM A2M_db_object: ' || SQLERRM);
+END;
+/   
+begin
+    EXECUTE IMMEDIATE 'drop public synonym A2M_assignment';
+exception
+    when others then
+    DOPL('DROP SYNONYM A2M_assignment: ' || SQLERRM);
+END;
+/    
+begin
+    EXECUTE IMMEDIATE 'create public synonym A2M_assignment  for ' || USER || '.A2M_assignment';
+exception
+    when others then
+    DOPL('CREATE SYNONYM A2M_assignment: ' || SQLERRM);
+END;
 /
 
 -- permissions
@@ -28,7 +70,7 @@ end;
 GRANT EXECUTE ON A2M_CLEAN_UP TO PUBLIC;
 GRANT EXECUTE ON A2M_pkg_assignment TO PUBLIC;
 GRANT EXECUTE ON A2M_MARK_DS_ASS2 TO PUBLIC;
-GRANT EXECUTE ON A2M_Host_User TO PUBLIC;
+--GRANT EXECUTE ON A2M_Host_User TO PUBLIC;
 GRANT SELECT ON A2M_DB_OBJECT TO PUBLIC;
 GRANT SELECT ON A2M_assignment TO PUBLIC;
 
