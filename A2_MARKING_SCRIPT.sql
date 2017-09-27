b9a7925516704e2b0e33d98a7a5685aaff54a3fe
Tim Baird <tim@timbaird.com>
2017-09-27 17:11:53 +1000

    clean up of  A2_MARKING_SCRIPT.sql

--- A2_MARKING_SCRIPT.sql
+++ A2_MARKING_SCRIPT.sql
@@ -2,15 +2,12 @@
 
 CLEAR SCREEN;
 
-exec tbaird.A2M_CLEAN_UP;
+exec A2M_CLEAN_UP;
 
-@./A2_SQLCODE.sql;
+@./Ass2_DW_Creation_Script.SQL;
  
 CLEAR SCREEN;
 
--- pre mark check for testing only
-exec tbaird.A2M_pkg_assignment.premark_CHECK('DS Ass2', USER, false);
-
-exec tbaird.A2M_Mark_Ds_Ass2( USER, FALSE);
+exec A2M_Mark_Ds_Ass2( USER, FALSE );
 
 
